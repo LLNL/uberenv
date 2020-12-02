81369d933ea1c9737c862f910d6a5fcfdfb875a2
Adrien M. Bernede <bernede1@llnl.gov>
2020-12-01 16:04:06 -0800

    Make mirror configurable in spack_config

--- uberenv.py
+++ uberenv.py
@@ -443,17 +443,23 @@ class SpackEnv(UberEnv):
         # copy in "defaults" config.yaml
         config_yaml = os.path.abspath(pjoin(self.uberenv_path,"spack_configs","config.yaml"))
         sexe("cp {} {}/".format(config_yaml, spack_etc_defaults_dir ), echo=True)
+        mirrors_yaml = os.path.abspath(pjoin(self.uberenv_path,"spack_configs","mirrors.yaml"))
+        sexe("cp {} {}/".format(mirrors_yaml, spack_etc_defaults_dir ), echo=True)
 
         # copy in other settings per platform
         if not cfg_dir is None:
             print("[copying uberenv compiler and packages settings from {0}]".format(cfg_dir))
 
             config_yaml    = pjoin(cfg_dir,"config.yaml")
+            mirrors_yaml    = pjoin(cfg_dir,"mirrors.yaml")
             compilers_yaml = pjoin(cfg_dir,"compilers.yaml")
             packages_yaml  = pjoin(cfg_dir,"packages.yaml")
 
             if os.path.isfile(config_yaml):
                 sexe("cp {} {}/".format(config_yaml , spack_etc_defaults_dir ), echo=True)
+
+            if os.path.isfile(mirrors_yaml):
+                sexe("cp {} {}/".format(mirrors_yaml , spack_etc_defaults_dir ), echo=True)
 
             if os.path.isfile(compilers_yaml):
                 sexe("cp {} {}/".format(compilers_yaml, spack_etc_defaults_dir ), echo=True)
