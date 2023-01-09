0e262595d1f698a34d10315c543fbb1a239f7c2a
Alex Tyler Chapman <chapman39@llnl.gov>
2023-01-09 12:22:00 -0800

    add src of test project to repo

---
+++ .ci/test-project/src/rzgenie-toss_3_x86_64_ib-gcc@4.9.3.cmake
@@ -0,0 +1,40 @@
+#------------------------------------------------------------------------------
+# !!!! This is a generated file, edit at own risk !!!!
+#------------------------------------------------------------------------------
+# CMake executable path: /usr/tce/bin/cmake
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Compilers
+#------------------------------------------------------------------------------
+# Compiler Spec: gcc@4.9.3
+#------------------------------------------------------------------------------
+if(DEFINED ENV{SPACK_CC})
+
+  set(CMAKE_C_COMPILER "/usr/WS2/meemee/uberenv/.ci/test-project/uberenv_libs/spack/lib/spack/env/gcc/gcc" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/WS2/meemee/uberenv/.ci/test-project/uberenv_libs/spack/lib/spack/env/gcc/g++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/WS2/meemee/uberenv/.ci/test-project/uberenv_libs/spack/lib/spack/env/gcc/gfortran" CACHE PATH "")
+
+else()
+
+  set(CMAKE_C_COMPILER "/usr/tce/bin/gcc" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/tce/bin/g++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/tce/bin/gfortran" CACHE PATH "")
+
+endif()
+
+#------------------------------------------------------------------------------
+# Hardware
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# TPLs
+#------------------------------------------------------------------------------
+
+set(HDF5_DIR "/usr/WS2/meemee/uberenv/.ci/test-project/uberenv_libs/spack/opt/spack/linux-rhel7-broadwell/gcc-4.9.3/hdf5-1.12.2-yekzpexggycpzywelldccl6w5wuvyfka" CACHE PATH "")
+
+
