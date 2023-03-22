5686793e16d91b90caf97208af1f98b50d7d34fc
Chris White <white238@llnl.gov>
2023-03-22 16:23:22 -0700

    update copyright

--- gen_spack_env_script.py
+++ gen_spack_env_script.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2015-2022, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2015-2023, Lawrence Livermore National Security, LLC.
 #
 # Produced at the Lawrence Livermore National Laboratory
 #
