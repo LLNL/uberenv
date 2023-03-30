e15d1feb07f9d9d15d4a7d9c06dedba21d28841d
Alex Tyler Chapman <100869159+chapman39@users.noreply.github.com>
2023-03-30 15:25:25 -0700

    Update uberenv.py
    Co-authored-by: Chris White <white238@llnl.gov>

--- uberenv.py
+++ uberenv.py
@@ -776,7 +776,7 @@ class SpackEnv(UberEnv):
 
     # Extract the first line of the full spec
     def read_spack_full_spec(self,pkg_name,spec):
-        res, out = sexe("{0} spec {2}".format(self.spack_exe(), self.pkg_name_with_spec), ret_output=True)
+        res, out = sexe("{0} spec {1}".format(self.spack_exe(), self.pkg_name_with_spec), ret_output=True)
         for l in out.split("\n"):
             if l.startswith(pkg_name) and l.count("@") > 0 and l.count("arch=") > 0:
                 return l.strip()
